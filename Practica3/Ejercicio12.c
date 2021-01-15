b666cf1a455f03816f5da65264346fba3621edc4
Hokuto02 <raquelramos422@gmail.com>
2021-01-15 17:10:28 +0100

    Ejercicio 12

---
+++ Practica3/Ejercicio12.c
@@ -0,0 +1,55 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int contSIGINT = 0;
+int contSIGTSTP = 0;
+
+void manejador(int signal){
+    switch(signal){
+        case SIGINT:
+            contSIGINT++;
+        break;
+        case SIGTSTP:
+            contSIGTSTP++;
+        break;
+    }
+
+}
+
+int main(int argc, char *argv[]){
+    // SIGINT
+    struct sigaction actSIGINT;
+    if(sigaction(SIGINT, NULL, &actSIGINT) == -1){
+        perror("Error en sigaction SIGINT, al sacar la accion");
+        return -1;
+    }
+    actSIGINT.sa_handler = manejador;
+    if(sigaction(SIGINT, &actSIGINT, NULL) == -1){
+        perror("Error en sigaction SIGINT, al poner la accion");
+        return -1;
+    }
+    // SIGTSTP
+    struct sigaction actSIGTSTP;
+    if(sigaction(SIGTSTP, NULL, &actSIGTSTP) == -1){
+        perror("Error en sigaction SIGTSTP, al sacar la accion");
+        return -1;
+    }
+    actSIGINT.sa_handler = manejador;
+    if(sigaction(SIGTSTP, &actSIGINT, NULL) == -1){
+        perror("Error en sigaction SIGTSTP, al poner la accion");
+        return -1;
+    }
+
+    sigset_t mask;
+    sigemptyset(&mask);
+    while(contSIGINT + contSIGTSTP < 10){
+        sigsuspend(&mask);
+    }
+
+    printf("Se ha recibido la señal SIGINT %i veces\n", contSIGINT);
+    printf("Se ha recibido la señal SIGTSTP %i veces\n", contSIGTSTP);
+
+    return 0;
+}
